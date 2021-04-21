4cc6f3e383fa727b0a65ebe8ce74d8a53e3fc1e2
zlobin <zbinorama@gmail.com>
2021-04-21 15:13:03 +0300

    add test create for user's roles

--- test/factories/users.rb
+++ test/factories/users.rb
@@ -6,5 +6,15 @@ FactoryBot.define do
     email
     avatar
     type { "" }
+
+    factory :developer do
+      type { "Developer" }
+    end
+    factory :manager do
+      type { "Manager" }
+    end
+    factory :admin do
+      type { "Admin" }
+    end
   end
 end
