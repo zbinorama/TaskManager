2d9816670b445d3ce35ac1e5c26b09474b0a020d
zlobin <zbinorama@gmail.com>
2021-05-29 11:11:22 +0300

    add index method; add respond_to json format

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -1,2 +1,11 @@
 class Api::V1::TasksController < Api::V1::ApplicationController
+  def index
+    tasks = Task.all.
+      ransack(ransack_params).
+      result.
+      page(page).
+      per(per_page)
+
+    respond_with(tasks, each_serializer: TaskSerializer, root: 'items', meta: build_meta(tasks))
+  end
 end
